f5342ff08f3010601b1c56cf0ab036274ab941a4
Calloga <papeabdoucalloga98@gmail.com>
2023-03-20 13:40:25 +0000

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const redux = require("redux");
+const create_store = redux.createStore;
 const CAKE_ORDERED = "CAKE_ORDERED";
 
 //action
@@ -11,7 +13,6 @@ function orderCake() {
 // initial state
 const initialState = {
   numOfCakes: 10,
-  anotherProperty: 0,
 };
 
 //reducer
@@ -21,9 +22,20 @@ const reducer = (state = initialState, action) => {
     case CAKE_ORDERED:
       return {
         ...state,
-        numOfCakes: numOfCakes - 1,
+        numOfCakes: state.numOfCakes - 1,
       };
     default:
       return state;
   }
 };
+
+//store
+
+const store = create_store(reducer);
+console.log("InitialState = " + store.getState());
+const unsub = store.subscribe(() => console.log(store.getState()));
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsub();
